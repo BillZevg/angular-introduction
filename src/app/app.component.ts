165b15b46256e368a48483a0e3b754f88eaa86fb
BillZevg <bzevgitis@gmail.com>
2023-12-02 22:08:32 +0200

    Person card, custom events and  handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,20 @@ import { Person } from './interfaces/person';
 import { PersonComponent } from './person/person.component';
 import { PersonAltComponent } from './person-alt/person-alt.component';
 import { EventBindComponent } from './event-bind/event-bind.component';
+import { OutputDemoComponent } from './output-demo/output-demo.component';
+import { PersonCardComponent } from './person-card/person-card.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PersonComponent, PersonAltComponent, EventBindComponent],
+  imports: [CommonModule,
+            RouterOutlet,
+            PersonComponent,
+            PersonAltComponent, 
+            EventBindComponent,
+            OutputDemoComponent,
+            PersonCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -38,6 +46,7 @@ export class AppComponent {
   
 users: Person[] = [
   {
+    photoURL: 'https://i.pravatar.cc/?img=1',
     givenName: 'John',
     surName: 'Doe',
     age: 30,
@@ -45,6 +54,7 @@ users: Person[] = [
     address: '123 Main St',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=2',
     givenName: 'Jane',
     surName: 'Doe',
     age: 28,
@@ -52,6 +62,7 @@ users: Person[] = [
     address: '123 Main St',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=3',
     givenName: 'Jim',
     surName: 'Brown',
     age: 45,
@@ -59,6 +70,7 @@ users: Person[] = [
     address: '456 Park Ave',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=4',
     givenName: 'Jill',
     surName: 'Brown',
     age: 42,
@@ -66,6 +78,7 @@ users: Person[] = [
     address: '456 Park Ave',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=5',
     givenName: 'Jake',
     surName: 'Smith',
     age: 36,
@@ -73,6 +86,7 @@ users: Person[] = [
     address: '789 Broadway',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=6',
     givenName: 'Judy',
     surName: 'Smith',
     age: 34,
@@ -80,6 +94,7 @@ users: Person[] = [
     address: '789 Broadway',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=7',
     givenName: 'Jack',
     surName: 'Johnson',
     age: 50,
@@ -87,6 +102,7 @@ users: Person[] = [
     address: '321 Oak St',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=8',
     givenName: 'Julie',
     surName: 'Johnson',
     age: 48,
@@ -94,6 +110,7 @@ users: Person[] = [
     address: '321 Oak St',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=9',
     givenName: 'Jerry',
     surName: 'Davis',
     age: 55,
@@ -101,13 +118,25 @@ users: Person[] = [
     address: '654 Pine St',
   },
   {
+    photoURL: 'https://i.pravatar.cc/?img=10',
     givenName: 'June',
     surName: 'Davis',
     age: 53,
     email: 'june.davis@example.com',
     address: '654 Pine St',
   },
-]
+];
+
+sendUser: Person | undefined;
+
+onDeleteUser(i: number) {
+  this.users.splice(i, 1);
+}
+
+onSendUser(user:Person) {
+  console.log(user);
+this.sendUser = user;
+}
 
 
 }
